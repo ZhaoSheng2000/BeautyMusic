44f925bb71827489b3e15f96c8a509cf644cb1ec
zhaosheng <zs17337825170@163.com>
2021-04-29 16:53:22 +0800

    update react+antd part

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Button } from 'antd';
 import './App.less';
 const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
+  <div>
+   
   </div>
 );
 
